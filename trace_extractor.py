73735558e8d219b24ff903b1f0a6759d300d7fcd
Deyang Zhao <zhaodeyang8811@gmail.com>
2025-04-10 10:40:28 -0700

    add debug flag

--- trace_extractor.py
+++ trace_extractor.py
@@ -2,5 +2,7 @@ class PCBTraceExtractor:
     def load_json_file(self, file_path):
         pass
 
-    def extract_traces_between_pads(self, designator_1, pad_num_1, designator_2, pad_num_2):
+    def extract_traces_between_pads(
+        self, designator_1, pad_num_1, designator_2, pad_num_2, debug=False
+    ):
         pass
